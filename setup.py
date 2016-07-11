886a0f86a52fcbf11a7e86e3ed7926be5bc88f04
Xu Wang <xuwang@gmail.com>
2016-07-10 23:30:41 -0700

    add author and license info to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='boxcli',
     version='0.1',
+    author = "Xu Wang",
+    author_email = "xuwang@gmail.com",
+    description = ("A command line utility for Box."),
+    license = "MIT",
+    keywords = "Box cli command",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
