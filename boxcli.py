1d3d049f6280e5b814cd9d5fa1925982530dc03a
Xu Wang <xuwang@gmail.com>
2016-07-10 23:29:46 -0700

    add main to boxcli.py

--- boxcli.py
+++ boxcli.py
@@ -1,3 +1,6 @@
+####################################################################
+# Command line utility for Box
+####################################################################
 import click
 
 from cmd_user import cmd_user
@@ -5,3 +8,6 @@ from cmd_folder import cmd_folder
 from cmd_file import cmd_file
 
 boxcli = click.CommandCollection(sources=[cmd_user, cmd_folder, cmd_file])
+
+if __name__ == '__main__':
+	boxcli()
